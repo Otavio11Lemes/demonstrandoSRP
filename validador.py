abed449bcf213592f136d047c4de53845baee238
Otavio Lemes <otavio11lemes@outlook.com>
2024-06-13 14:17:22 -0300

    questao 6

--- validador.py
+++
@@ -1,8 +0,0 @@
-def validate_user_data(data):
-    if not data.get('name') or not data.get('email') or not data.get('password'):
-        return 'Missing data'
-
-    if len(data['password']) < 6:
-        return 'Password too short'
-
-    return None
